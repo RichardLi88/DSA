04dc9f0a12f28af6219d49b5e24236ca1820ae46
Richard <rlii0102@student.monash.edu>
2025-03-06 23:22:20 +1100

    Implement selection sort
    - implemented the standard selection sort off by memory

---
+++ selection_sort.py
@@ -0,0 +1,19 @@
+import math
+from typing import List
+
+
+def selection_sort(unsorted_list: List[int]):
+    pointer = 0
+    for i in range(len(unsorted_list) - 1):
+        cur_min = math.inf
+        index = None
+        for j in range(i, len(unsorted_list)):
+            if unsorted_list[j] < cur_min:
+                cur_min = unsorted_list[j]
+                index = j
+        unsorted_list[i], unsorted_list[index] = unsorted_list[index], unsorted_list[i]
+
+
+random_list = [5, 4, 3, 2, 1]
+selection_sort(random_list)
+print(random_list)
