99792814e64cd9ef6356826b987988c92f08808b
Richard <rlii0102@student.monash.edu>
2025-03-09 02:00:12 +1100

    code in-place merge sort
    - implement in place merge sort in python

--- merge_sort.py
+++ merge_sort.py
@@ -1,27 +1,38 @@
 from typing import List
 
-def merge_sort(unsorted_list: List[int],left,mid,right):
-    
+
+def merge_sort(unsorted_list: List[int], left, right):
+    if left < right:
+        mid = (left + right) // 2
+        merge_sort(unsorted_list, left, mid)
+        merge_sort(unsorted_list, mid + 1, right)
+        merge(unsorted_list, left, mid, right)
 
 
-def merge(list1: List[int], list2: List[int]):
-    new_list = []
+def merge(unsorted_list, start, mid, end):
+    start2 = mid + 1
+    if unsorted_list[start2] >= unsorted_list[mid]:
+        return
 
-    p1,p2 = 0,0
-
-    while p1 < len(list1) and p2 < len(list2):
-        if p1 <= p2:
-            new_list.append(list1[p1])
-            p1 += 1
+    while start <= mid and start2 <= end:
+        if unsorted_list[start] <= unsorted_list[start2]:
+            start += 1
         else:
-            new_list.append(list2[p2]))
-            p2 += 1 
+            current, value = start2, unsorted_list[start2]
 
-    while p1 < len(list1):
-        new_list.append(list1[p1])
-        p1 +=1 
-    
-    while p2 < len(list2):
-        new_list.append(list2[p2])
-        p2 += 1 
-    return new_list
+            while current != start:
+                unsorted_list[current] = unsorted_list[current - 1]
+                current -= 1
+
+            unsorted_list[start] = value
+
+            start += 1
+            start2 += 1
+            mid += 1
+        print(unsorted_list)
+
+
+if __name__ == "__main__":
+    unsorted_list = [5, 4, 3, 2, 1]
+    merge_sort(unsorted_list, 0, len(unsorted_list) - 1)
+    print(unsorted_list)
