8e6a76eb007dfb711523ed2668964a751b799902
Richard <rlii0102@student.monash.edu>
2025-03-09 01:18:35 +1100

    Complete z_algorithm implementation
    - implement linear time pattern matching algorithm

--- z_algorithm.py
+++ z_algorithm.py
@@ -1,29 +1,41 @@
 def z_algorithm(txt: str, pat: str):
-    z_box = [0] * len(pat)
+    joined_str = pat + "$" + txt
+    z_box = [0] * len(joined_str)
+    # l is the start of the substring with the highest right index that matches the start of the list
+    # r is the right most index of the substring that matches the start of the list so far
     l, r = 0, 0
     z_box[0] = 1
-    for i in range(1, len(pat)):
-        if r < i:
-            k, current_length = i, 0
-            while (
-                k + current_length < len(pat)
-                and pat[current_length] == pat[k + current_length]
-            ):
-                r = k + current_length
-                current_length += 1
-            if current_length > 0:
-                r = k + current_length - 1
-                l = k
-                z_box[k] = current_length - 1
+    print(joined_str[1] == joined_str[0])
+    for i in range(1, len(z_box)):
+        print(joined_str)
+        k = i
+        length = 0
+        if r > i:
+            if z_box[k - l] >= r:
+                while k < len(z_box) and joined_str[k] == joined_str[length]:
+                    length += 1
+                    k += 1
+                z_box[i] = k - 1
+                l = i
+                r = k - 1
+
             else:
-                z_box[k] = z_box[k - 1]
-        elif z_box[k - l] + k < r:
-            z_box[k] = z_box[k - l]
+                z_box[i] = z_box[i - l]
         else:
-            k, current_length = i, z_box[k - l]
-            while k + current_length < len(pat):
-                r = k + current_length
-                if r == len(pat):
-                    break
-                if pat[r + 1] == pat[k - l + current_length + 1]:
-                    r += 1
+            while k < len(joined_str) and joined_str[k] == joined_str[length]:
+                length += 1
+                k += 1
+            if length == 0:
+                z_box[i] = 0
+            else:
+                l = i
+                r = k - 1
+                z_box[i] = k - l
+        print(z_box)
+    return z_box
+
+
+if __name__ == "__main__":
+    test_str = "abcd"
+    txt = "aabcuiuabcd"
+    print(z_algorithm(txt, test_str))
